6dab33226988448a4070b8cb03955df3caa5c67b
John Ross <jlross42@gmail.com>
2018-11-10 22:50:00 -0800

    spacing

--- github-enhancements.user.js
+++ github-enhancements.user.js
@@ -4,9 +4,9 @@
 // @version      0.3
 // @description  Improve Yo Self
 // @author       turkoid
-// @include      /^https?://(.*\.)?github\.(.*\.)com.*$/
+// @include      /^https?://(.*\.)?github\.(.*\.)?com.*$/
 // @updateURL    https://raw.githubusercontent.com/turkoid/github-enhacements/master/github-enhancements.meta.js
-// @downloadURL    https://raw.githubusercontent.com/turkoid/github-enhacements/master/github-enhancements.user.js
+// @downloadURL  https://raw.githubusercontent.com/turkoid/github-enhacements/master/github-enhancements.user.js
 // @grant        none
 // ==/UserScript==
 
