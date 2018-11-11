7f9abcefbfa6f07ba8cdbd3ccaef9dec31c5ad57
John Ross <jlross42@gmail.com>
2018-11-10 22:47:08 -0800

    better url matching

--- github-enhancements.meta.js
+++ github-enhancements.meta.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name         Github Enhancements
 // @namespace    turkoid
-// @version      0.2
+// @version      0.3
 // @description  Improve Yo Self
 // @author       turkoid
-// @include      /^https?://(.*\.)?github\.(.*\.)com.*$/
+// @include      /^https?://(.*\.)?github\.(.*\.)?com.*$/
+// @updateURL    https://raw.githubusercontent.com/turkoid/github-enhacements/master/github-enhancements.meta.js
+// @downloadURL    https://raw.githubusercontent.com/turkoid/github-enhacements/master/github-enhancements.user.js
 // @grant        none
 // ==/UserScript==
